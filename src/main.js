e1d148663f1956917c9dda553780b1df9e152f7c
Thomas Margosian <tmargo101@gmail.com>
2020-12-05 21:09:55 -0500

    - Added basic save states. - Refined some search terms. - Added falcon heavy listing. - Added database starting terms - Added Ideas to README.md

--- src/main.js
+++ src/main.js
@@ -2,52 +2,73 @@ import * as map from './map.js';
 import * as ajax from './ajax.js';
 
 let sats;
-let boosters;
-let launches;
-let capsules;
-let dragons;
+let currentState = {
+	prefix: "txm5483-",
+	searchTerm: null,
 
-
+};
 let endpoints = {
 	getAllStarlinkSats: "https://api.spacexdata.com/v3/starlink",
 	getAllPastLaunches: "https://api.spacexdata.com/v3/launches/past",
 	getAllCores: "https://api.spacexdata.com/v3/cores",
 	getCapsule: ""
-
 }
 
+let getSavedState = () => {
+	currentState.searchTerm = localStorage.getItem(currentState.prefix+"searchTerm");
+
+	// if we find a previously set name value, display it
+	if (currentState.searchTerm){
+		searchField.value = currentState.searchTerm;
+	}
+};
+
 let init = () => {
+	getSavedState();
+
 	map.initMap();
 	map.addMarkersToMap();
+	drawDatabaseHome();
 	setupUI();
 };
 
+let displayLoading = (div) => {
+	document.querySelector(div).innerHTML = "<h3 class='text-center'>Loading...</h3>";
+}
+
 let setupUI = () => {
+
+	// Save state handlers
+	searchField.onchange = (e) => {
+		localStorage.setItem(currentState.prefix+"searchTerm", e.target.value);
+	};
+
+
 	starlinkNavItem.onclick = () => {
 		if (!sats) {
 			loadSats();
 		}
-};
+	};
 
 	falcon1HistoryNav.onclick = () => {
+		displayLoading('#databaseDiv');
 		let displayLaunchHistory = (jsonString) => {
-		let allLaunches = JSON.parse(jsonString);
-		let headerColumns = ["Launch Name", "Success", "Launch Details"];
-		drawDatabaseTable(headerColumns);
-		for (let launch of allLaunches) {
-			if (launch.flight_number < 6) {
+			let allLaunches = JSON.parse(jsonString);
+			let headerColumns = ["Launch Name", "Success", "Launch Details"];
+			drawDatabaseTable(headerColumns);
+			for (let launch of allLaunches) {
 				drawDatabaseRow([
-					launch.name,
-					launch.success,
+					`<a href="#">${launch.mission_name}</a>`,
+					launch.launch_success,
 					launch.details
 				])
 			}
-}
 		}
-		ajax.getData(endpoints.getAllPastLaunches, displayLaunchHistory);
+		ajax.getData(endpoints.getAllPastLaunches + "/?rocket_id=falcon1", displayLaunchHistory);
 	}
 
 	falcon9HistoryNav.onclick = () => {
+		displayLoading('#databaseDiv');
 		let displayLaunchHistory = (jsonString) => {
 			let allLaunches = JSON.parse(jsonString);
 			let headerColumns = ["Launch Name", "Success", "Launch Details", "Booster"];
@@ -58,6 +79,7 @@ let setupUI = () => {
 					launch.launch_success,
 					launch.details,
 					`<a href="#">${launch.rocket.first_stage.cores[0].core_serial}</a>`
+
 				])
 
 			}
@@ -65,7 +87,26 @@ let setupUI = () => {
 		ajax.getData(endpoints.getAllPastLaunches+"/?rocket_id=falcon9", displayLaunchHistory);
 	}
 
-	cargoDragonHistoryNav.onclick = () => {
+	falconHeavyHistoryNav.onclick = () => {
+		displayLoading('#databaseDiv');
+		let displayLaunchHistory = (jsonString) => {
+			let allLaunches = JSON.parse(jsonString);
+			let headerColumns = ["Launch Name", "Success", "Launch Details"];
+			drawDatabaseTable(headerColumns);
+			for (let launch of allLaunches) {
+				drawDatabaseRow([
+					launch.mission_name,
+					launch.launch_success,
+					launch.details
+				])
+			}
+		}
+		ajax.getData(endpoints.getAllPastLaunches+"/?rocket_id=falconheavy", displayLaunchHistory);
+	}
+
+
+	dragonHistoryNav.onclick = () => {
+		displayLoading('#databaseDiv');
 		let displayLaunchHistory = (jsonString) => {
 			let allLaunches = JSON.parse(jsonString);
 			let headerColumns = ["Launch Name", "Success", "Launch Details"];
@@ -74,28 +115,27 @@ let setupUI = () => {
 				if (launch.capsules[0]) {
 
 					drawDatabaseRow([
-						launch.name,
-						launch.success,
+						launch.mission_name,
+						launch.launch_success,
 						launch.details
 					])
 				}
 			}
 		}
 		ajax.getData(endpoints.getAllPastLaunches, displayLaunchHistory);
-
 	}
-
 };
 
 let drawDatabaseTable = (inColumns) => {
 	let thisTable = `
-	<table id="currentDatabaseDivTable" class="table table-responsive" data-sticky-header="true" style="overflow-y:scroll; max-height: 80vh;">
+	<table id="currentDatabaseDivTable" class="table table-responsive" style="overflow-y:scroll; max-height: 80vh;">
 		<thead class="thead-dark">
 			<tr>`;
 
 	for (let column of inColumns) {
 		thisTable += `
-		<th>${column}</th>`;
+		<th>${column}</th>
+		`;
 	}
 
 	thisTable +=`
@@ -120,13 +160,41 @@ let drawDatabaseRow = (inColumns, inClass = 'table-light') => {
 			}
 		}
 		thisRow += `</tr>`
-		currentDatabaseDivTable.innerHTML += thisRow;
+		document.querySelector("tbody").innerHTML += thisRow;
 	}
 }
 
 let drawPopup = () => {
 
 }
+
+let drawDatabaseHome = () => {
+	databaseDiv.innerHTML = `
+	<div class="text-center">
+		<button class="mt-5 mb-3 btn btn-outline-primary btn-lg" id="falcon1HistoryNav">Falcon 1 Launches</button><br>
+		<button class="m-3 btn btn-outline-primary btn-lg" id="falcon9HistoryNav">Falcon 9 Launches</button><br>
+		<button class="m-3 btn btn-outline-primary btn-lg" id="falconHeavyHistoryNav">Falcon Heavy Launches</button><br>
+		<button class="m-3 btn btn-outline-primary btn-lg" id="dragonHistoryNav">Dragon Capsule Launches</button><br>
+
+	</div>
+	<div class="text-center mt-5">
+		<h3 class="mb-3">Next Launch</h3>
+		<p>ARABSAT</p>
+		<p>Nov 20</p>
+	</div>
+	<div class="text-center mt-5">
+		<h3 class="mb-3">Last Launch</h3>
+		<p>Starlink</p>
+		<p>Nov 10</p>
+	</div>
+	`
+};
+
+let drawDatabseControls = () => {
+	databaseControls.innerHTML = `
+	<button>
+	`
+};
 
 let loadSats = () => {
 	const url = endpoints.getAllStarlinkSats;
