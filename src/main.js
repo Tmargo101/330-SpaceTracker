58580e28138b346599ffb27cec5947fb5ef68468
Tom Margosian <tommargosian@toms-macbook-air.local>
2020-12-06 17:54:22 +0000

    - Replaced DivFlip utility method with hideDiv and showDiv methods (Extract these into a utility class)
    - Added drawDetailsView
    
    - Added click handlers to all links (Falcon 1, Falcon 9, Falcon Heavy)

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ let currentState = {
 let endpoints = {
 	getAllStarlinkSats: "https://api.spacexdata.com/v4/starlink",
 	getAllPastLaunches: "https://api.spacexdata.com/v3/launches/past",
+	getOneLaunch: "https://api.spacexdata.com/v3/launches/",
 	getAllCores: "https://api.spacexdata.com/v3/cores",
 	getCapsule: ""
 }
@@ -49,9 +50,20 @@ let setupUI = () => {
 		}
 	};
 		
-	databaseBackButton.onclick = () => {
-		divFlip("databaseDiv", "databaseHomeDiv");
+	databaseHomeButton.onclick = () => {
+		hideDiv("databaseDiv");
+		
+		showDiv("databaseHomeDiv");
 	}
+	
+	databaseBackButton.onclick = () => {
+		hideDiv("databaseDetailsView");
+		hideDiv("databaseHomeDiv");
+		
+		showDiv("databaseDiv");
+
+	}
+
 
 	falcon1HistoryNav.onclick = () => {
 		displayLoading('#databaseDiv');
@@ -61,14 +73,23 @@ let setupUI = () => {
 			drawDatabaseTable(headerColumns);
 			for (let launch of allLaunches) {
 				drawDatabaseRow([
-`<a href="#" data-toggle="modal" data-target="#flightDetailsDiv${launch.flight_number}">${launch.mission_name}</a>`,
+					`<a href="#" class="getFlightDetails" data-value="${launch.flight_number}">${launch.mission_name}</a>`,
 					launch.launch_success,
 					launch.details,
 					
 				],launch)
 			}
+			let detailsLinks = document.querySelectorAll('.getFlightDetails');
+			detailsLinks.forEach(link =>{
+				link.onclick = (e) => {
+					console.log(e.target.dataset.value);
+					drawDetailsView(e.target.dataset.value);
+				}
+			});
+
 		}
-		divFlip("databaseHomeDiv", "databaseDiv");
+		hideDiv("databaseHomeDiv");
+		showDiv("databaseDiv");
 		ajax.getData(endpoints.getAllPastLaunches + "/?rocket_id=falcon1", displayLaunchHistory);
 	}
 
@@ -80,7 +101,7 @@ let setupUI = () => {
 			drawDatabaseTable(headerColumns);
 			for (let launch of allLaunches) {
 				drawDatabaseRow([
-					`<a href="#" data-toggle="modal" data-target="#flightDetailsDiv${launch.flight_number}">${launch.mission_name}</a>`,
+					`<a href="#" class="getFlightDetails" data-value="${launch.flight_number}">${launch.mission_name}</a>`,
 					launch.launch_success,
 					launch.details,
 					launch.rocket.first_stage.cores[0].core_serial
@@ -88,8 +109,18 @@ let setupUI = () => {
 
 				],launch);
 			}
+			
+			let detailsLinks = document.querySelectorAll('.getFlightDetails');
+			detailsLinks.forEach(link =>{
+				link.onclick = (e) => {
+					console.log(e.target.dataset.value);
+					drawDetailsView(e.target.dataset.value);
+				}
+			});
+
 		}
-		divFlip("databaseHomeDiv", "databaseDiv");
+		hideDiv("databaseHomeDiv");
+		showDiv("databaseDiv");
 		ajax.getData(endpoints.getAllPastLaunches+"/?rocket_id=falcon9", displayLaunchHistory);
 	}
 
@@ -101,13 +132,22 @@ let setupUI = () => {
 			drawDatabaseTable(headerColumns);
 			for (let launch of allLaunches) {
 				drawDatabaseRow([
-					`<a href="#" data-toggle="modal" data-target="#flightDetailsDiv${launch.flight_number}">${launch.mission_name}</a>`,
+					`<a href="#" class="getFlightDetails" data-value="${launch.flight_number}">${launch.mission_name}</a>`,
 					launch.launch_success,
 					launch.details
 				],launch)
 			}
+			let detailsLinks = document.querySelectorAll('.getFlightDetails');
+			detailsLinks.forEach(link =>{
+				link.onclick = (e) => {
+					console.log(e.target.dataset.value);
+					drawDetailsView(e.target.dataset.value);
+				}
+			});
+
 		}
-		divFlip("databaseHomeDiv", "databaseDiv");
+		hideDiv("databaseHomeDiv");
+		showDiv("databaseDiv");
 		ajax.getData(endpoints.getAllPastLaunches+"/?rocket_id=falconheavy", displayLaunchHistory);
 	}
 
@@ -168,49 +208,65 @@ let drawDatabaseRow = (inColumns, thisLaunch = {}, inClass = 'table-light') => {
 			thisRow += `<td>No Information Provided</td>`
 			}
 		}
-		thisRow += `</tr>
-		<!-- Modal -->
-		<div class="modal fade" id="flightDetailsDiv${thisLaunch.flight_number}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-		  <div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-			  <div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLongTitle">${thisLaunch.mission_name}</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				  <span aria-hidden="true">&times;</span>
-				</button>
-			  </div>
-			  <div class="modal-body">
-				<p>Details: ${thisLaunch.details}</p>
-				<a href="${thisLaunch.links.video_link}">Link to Launch Video</a>
-				
-			  </div>
-			</div>
-		  </div>
-		</div>
-		`
+		// thisRow += `</tr>
+		// <!-- Modal -->
+		// <div class="modal fade" id="flightDetailsDiv${thisLaunch.flight_number}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		//   <div class="modal-dialog modal-dialog-centered" role="document">
+		// 	<div class="modal-content">
+		// 	  <div class="modal-header">
+		// 		<h5 class="modal-title" id="exampleModalLongTitle">${thisLaunch.mission_name}</h5>
+		// 		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		// 		  <span aria-hidden="true">&times;</span>
+		// 		</button>
+		// 	  </div>
+		// 	  <div class="modal-body">
+		// 		<p>Details: ${thisLaunch.details}</p>
+		// 		<a href="${thisLaunch.links.video_link}">Link to Launch Video</a>
+		// 		
+		// 	  </div>
+		// 	</div>
+		//   </div>
+		// </div>
+		// `
 		console.log(thisRow);
 		document.querySelector("tbody").innerHTML += thisRow;
 	}
 }
 
-let divFlip = (divToHide, divToShow) => {
-	console.log(divToHide);
-	document.querySelector(`#${divToHide}`).style.display = "none";
+// let divFlip = (divToHide, divToShow) => {
+// 	document.querySelector(`#${divToShow}`).style.display = "";
+// };
+
+let showDiv = (divToShow) => {
 	document.querySelector(`#${divToShow}`).style.display = "";
-// 	if (div1.style.display == "none"){
-// 		div2.style.display = "none";
-// 		div1.style.display = "";
-// 	} else if (div2.style.display == "none") {
-// 		div1.style.display = "none";
-// 		div2.style.display = "";
-// 
-// 	}
-};
+}
+
+let hideDiv = (divToHide) => {
+	document.querySelector(`#${divToHide}`).style.display = "none";
+}
 
 let drawDatabseControls = () => {
 	databaseControls.innerHTML = `
 	<button>
 	`
+};
+
+let drawDetailsView = (launchNumber) => {
+	document.querySelector("#databaseDetailsView").innerHTML += "";
+	hideDiv("databaseDiv");
+	showDiv("databaseDetailsView");
+	
+	let callback = (jsonString) => {
+		let launch = JSON.parse(jsonString);
+		console.log(launch);
+		let thisDetailsView = `
+		<div id="#thisDetailsView">
+			<p>${launch.mission_name}</p>
+		</div>
+		`;
+		document.querySelector("#databaseDetailsView").innerHTML += thisDetailsView;
+	};
+	ajax.getData(endpoints.getOneLaunch+launchNumber, callback);
 };
 
 let loadSats = () => {
